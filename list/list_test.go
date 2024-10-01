09bb3164a1ed7b8197e672e3042c65d801fddd7b
tyspice <tyner@tyspice.dev>
2024-09-30 17:46:07 -0700

    add test

--- list/list_test.go
+++ list/list_test.go
@@ -1,3 +1,22 @@
 package list
 
-// TODO: add tests
+import (
+	"slices"
+	"testing"
+)
+
+func TestAddingAndReading(t *testing.T) {
+	in := []string{"first", "second", "third"}
+	out := make([]string, 0)
+
+	testList := New(in[2])
+	testList.AddNode(in[1])
+	testList.AddNode(in[0])
+	testList.ForEachNode(func(s string) {
+		out = append(out, s)
+	})
+
+	if !slices.Equal(in, out) {
+		t.Errorf(`FAIL: expected %v but got %v`, in, out)
+	}
+}
